279ff7fe4863082ed50e1e7a607845e6227ec9e5
Nick Inman <ninman@Nicks-MacBook-Pro.local>
2019-06-05 14:46:04 -0700

    formatting parsed data

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,4 +1,31 @@
 class ResultsController < ApplicationController
+
   def index
   end
+
+  def create
+    @result = Result.find_or_create_by(resultId: result_params['resultId']) do |r|
+      r.resultId = result_param["resultId"]
+      r.name = result_param['name']
+      r.address = result_param['address']
+      r.postalCode = result_param['postalCode']
+      r.enhanced = result_param['enhanced']
+      r.careHome = result_param['careHome']
+      r.description = result_param['description']
+      r.averageCost = result_param['averageCost']
+      r.averageRating = result_param['averageRating']
+      r.foodRating = result_param['foodRating']
+      r.reviews = result_param['reviews']
+      r.url = result_param['url']
+      r.websiteUrl = result_param['websiteUrl']
+      r.phone = result_param['phone']
+    end
+  end
+
+  private
+
+  def result_params
+    params.require(:result).permit(:resultId, :name, :address, :postalCode, :enhanced, :careHome, :description, :averageCost, :averageRating, :foodRating, :review, :url, :websiteUrl, :phone)
+  end
+
 end
--- app/services/search_db.rb
+++ app/services/search_db.rb
@@ -1,7 +1,7 @@
 class SearchDb < ApplicationRecord
 
   def self.search(zip)
-    results = Array.new
+    results = []
     url = "#{Rails.root}/app/assets/data.json"
     @data = File.read(url)
     parsed = JSON.parse(@data)
@@ -14,8 +14,24 @@ class SearchDb < ApplicationRecord
   end
 
   def self.parse_search(results)
-    results.each do |result|
-      binding.pry
+    result_array = []
+    results.each do |data|
+      result = Result.new
+      result.resultId = data["resultId"]
+      result.name = data['name']
+      result.address = data['addressLine1']
+      result.postalCode = data['postalCode']
+      result.enhanced = data['isEnhanced']
+      result.careHome = data['isCareHome']
+      result.description = data['selfDescription']
+      result.averageCost = data['averageCost']
+      result.averageRating = data['averageRating']
+      result.foodRating = data['foodRating']
+      result.reviews = data['reviewsCount']
+      result.url = data['url']
+      result.websiteUrl = data['websiteUrl']
+      result.phone = data["primaryPhoneNumber"]["e164"]
+      result_array << result
     end
   end
 
